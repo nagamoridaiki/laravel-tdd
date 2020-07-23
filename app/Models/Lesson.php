21faef58bcd2c3ad702f42e470d85561364d6ec2
nagamoridaiki <no.swea1017@gmail.com>
2020-07-23 13:29:10 +0900

    フィーチャーテスト1回目

---
+++ app/Models/Lesson.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Lesson extends Model
+{
+    public function getVacancyLevelAttribute(): VacancyLevel
+        {
+            return new VacancyLevel($this->remainingCount());
+        }
+    
+    
+        private function remainingCount(): int
+        {
+            return 0;
+        }
+}
