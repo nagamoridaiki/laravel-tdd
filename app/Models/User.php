6514a86e9c96ae3084ea6e52f9c9f7f189008be1
nagamoridaiki <no.swea1017@gmail.com>
2020-07-23 21:11:55 +0900

    エラーメッセージの出力

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,11 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Carbon\Carbon;
 
 class User extends Authenticatable
 {
@@ -39,17 +39,6 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function canReserve(Lesson $lesson): bool
-    {
-        if ($lesson->remainingCount() === 0) {
-            return false;
-        }
-        if ($this->plan === 'gold') {
-            return true;
-        }
-        return $this->reservationCountThisMonth() < 5;
-    }
-
     public function reservations(): HasMany
     {
         return $this->hasMany(Reservation::class);
@@ -59,13 +48,25 @@ class User extends Authenticatable
     {
         $today = Carbon::today();
         return $this->reservations()
-            ->whereYear('created_at', $today->year)
-            ->whereMonth('created_at', $today->month)
+            ->whereRaw("DATE_FORMAT(created_at, '%Y%m') = ?", $today->format('Ym'))
             ->count();
-        // あるいは以下のようにひとまとめにしてもいいでしょう
-        // return $this->reservations()
-        //    ->whereRaw("DATE_FORMAT(created_at, '%Y%m') = ?", $today->format('Ym'))
-        //    ->count();
     }
 
+    /**
+     * @param Lesson $lesson
+     * @return void
+     * @throws \Exception
+     */
+    public function canReserve(Lesson $lesson): void
+    {
+        if ($lesson->remainingCount() === 0) {
+            throw new \Exception('レッスンの予約可能上限に達しています。');
+        }
+        if ($this->plan === 'gold') {
+            return;
+        }
+        if ($this->reservationCountThisMonth() === 5) {
+            throw new \Exception('今月の予約がプランの上限に達しています。');
+        }
+    }
 }
