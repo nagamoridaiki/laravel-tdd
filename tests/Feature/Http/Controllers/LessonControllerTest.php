188b4c833e1a20e060d8862902f307e978d4f6a2
nagamoridaiki <no.swea1017@gmail.com>
2020-07-24 12:32:30 +0900

    Lesson予約申し込み時のレスポンス（フィーチャーテスト）

--- tests/Feature/Http/Controllers/LessonControllerTest.php
+++ tests/Feature/Http/Controllers/LessonControllerTest.php
@@ -5,15 +5,18 @@ namespace Tests\Feature\Http\Controllers;
 use App\Models\Lesson;
 use App\Models\Reservation;
 use App\Models\User;
+use App\Models\UserProfile;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\Response;
+use Tests\Factories\Traits\CreatesUser;
 use Tests\TestCase;
 
 class LessonControllerTest extends TestCase
 {
 
     use RefreshDatabase;
+    use CreatesUser;
 
     /**
     * @param int $capacity
@@ -28,12 +31,13 @@ class LessonControllerTest extends TestCase
         $lesson = factory(Lesson::class)->create(['name' => '楽しいヨガレッスン', 'capacity' => $capacity]);
         for ($i = 0; $i < $reservationCount; $i++) {
             $user = factory(User::class)->create();
+            factory(UserProfile::class)->create(['user_id' => $user->id]);
             factory(Reservation::class)->create(['lesson_id' => $lesson->id, 'user_id' => $user->id]);
         }
 
-        $user = factory(User::class)->create();
+        $user = $this->createUser();
         $this->actingAs($user);
-        
+
         $response = $this->get("/lessons/{$lesson->id}");
 
         $response->assertStatus(Response::HTTP_OK);
